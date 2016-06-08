b951283026fb73353b81c5ea059055af75edd4b2
Raül Pérez <repejota@gmail.com>
2016-06-08 19:28:36 +0200

    Test TB suffix

--- prefix_test.go
+++ prefix_test.go
@@ -41,3 +41,19 @@ func TestGB(t *testing.T) {
 		})
 	})
 }
+
+func TestTB(t *testing.T) {
+	Convey("Scenario: Convert string with the prefix TB to number of megabytes", t, func() {
+		Convey("Given the string 1TB, the number of megabytes is 1048576", func() {
+			str := "1TB"
+			mb, _ := GetMB(str)
+			So(mb, ShouldEqual, 1048576)
+		})
+		Convey("Given the string 1tb, the number of megabytes is 0 as lower case denominations are not supported", func() {
+			str := "1tb"
+			mb, err := GetMB(str)
+			So(mb, ShouldEqual, 0)
+			So(err.Error(), ShouldEqual, "Unknown Denomination")
+		})
+	})
+}
