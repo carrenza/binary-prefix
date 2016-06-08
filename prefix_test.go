314aebea1b1044db8f795c1b009a55aa246d48ce
Raül Pérez <repejota@gmail.com>
2016-06-08 19:27:22 +0200

    Added tests for GB suffix

--- prefix_test.go
+++ prefix_test.go
@@ -25,3 +25,19 @@ func TestMB(t *testing.T) {
 		})
 	})
 }
+
+func TestGB(t *testing.T) {
+	Convey("Scenario: Convert string with the prefix GB to number of megabytes", t, func() {
+		Convey("Given the string 1GB, the number of megabytes is 1024", func() {
+			str := "1GB"
+			mb, _ := GetMB(str)
+			So(mb, ShouldEqual, 1024)
+		})
+		Convey("Given the string 1gb, the number of megabytes is 0 as lower case denominations are not supported", func() {
+			str := "1gb"
+			mb, err := GetMB(str)
+			So(mb, ShouldEqual, 0)
+			So(err.Error(), ShouldEqual, "Unknown Denomination")
+		})
+	})
+}
