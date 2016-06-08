c0a03c44f3c95f51403949a7be2ba930256e0247
Raül Pérez <repejota@gmail.com>
2016-06-08 19:33:51 +0200

    Test suffix ZB

--- prefix_test.go
+++ prefix_test.go
@@ -89,3 +89,19 @@ func TestEB(t *testing.T) {
 		})
 	})
 }
+
+func TestZB(t *testing.T) {
+	Convey("Scenario: Convert string with the prefix ZB to number of megabytes", t, func() {
+		Convey("Given the string 1ZB, the number of megabytes is 1125899906842624", func() {
+			str := "1ZB"
+			mb, _ := GetMB(str)
+			So(mb, ShouldEqual, 1125899906842624)
+		})
+		Convey("Given the string 1zb, the number of megabytes is 0 as lower case denominations are not supported", func() {
+			str := "1zb"
+			mb, err := GetMB(str)
+			So(mb, ShouldEqual, 0)
+			So(err.Error(), ShouldEqual, "Unknown Denomination")
+		})
+	})
+}
