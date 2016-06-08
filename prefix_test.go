6609df3325449b25ef38e5e56fb406100505c202
Raül Pérez <repejota@gmail.com>
2016-06-08 19:39:51 +0200

    Test invalid format

--- prefix_test.go
+++ prefix_test.go
@@ -10,6 +10,17 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+func TestInvalidFormat(t *testing.T) {
+	Convey("Scenario: Convert string with an invalid format to number of megabytes", t, func() {
+		Convey("Given the string 'foo', fails as it is an invalid format", func() {
+			str := "foo"
+			mb, err := GetMB(str)
+			So(mb, ShouldEqual, 0)
+			So(err.Error(), ShouldEqual, "strconv.ParseFloat: parsing \"f\": invalid syntax")
+		})
+	})
+}
+
 func TestMB(t *testing.T) {
 	Convey("Scenario: Convert string with the prefix MB to number of megabytes", t, func() {
 		Convey("Given the string 1MB, the number of megabytes is 1", func() {
